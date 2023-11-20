0a37ccb86ffde7e9ce689b32463673e3901bf6a8
sirluks <sirluks@gmail.com>
2023-11-19 23:16:23 -0300

    retornando aos trabalhos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Http::macro('punkapi', function () {
+            return Http::acceptJson()
+            ->baseUrl(config('punkapi.url'))
+            ->retry(3,100);
+        });
     }
 }
